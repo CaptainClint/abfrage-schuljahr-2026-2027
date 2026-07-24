771fc68f798a1cfdd32f226ab74a8813e9bc0acd
Christian Schulz <info@schulz-patent.de>
2026-07-24 09:14:31 +0100

    chore: set live Google Sheets endpoint URL

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 // Nach dem Veröffentlichen des Google Apps Script (google-apps-script/Code.gs)
 // als Web App die dabei erzeugte URL hier eintragen. Siehe
 // docs/superpowers/specs/2026-07-23-survey-form-design.md für die Einrichtung.
-export const SHEETS_ENDPOINT_URL = "https://script.google.com/macros/s/REPLACE_ME/exec";
+export const SHEETS_ENDPOINT_URL =
+  "https://script.google.com/macros/s/AKfycbzh7G7SuWR_PUN5ycWi-ahkNi9-8S_eE2IQdBsNPhIpfSbvL86icnCk2p5Bt21EL5D2/exec";
